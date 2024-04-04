7387566621cecac6e8695dd82f72fd31c992f81e
Tiger <1357958736@qq.com>
2024-04-04 17:24:32 +0800

    增加校验

--- remote-common/src/main/java/com/tiger/remote/spi/SpiInstance.java
+++ remote-common/src/main/java/com/tiger/remote/spi/SpiInstance.java
@@ -14,12 +14,18 @@ public class SpiInstance<T> {
         if (StringUtils.isEmpty(name)) {
             throw new IllegalArgumentException("Extension name == null");
         }
+        if (instanceMap.get(name) == null) {
+            throw new RuntimeException("instance name not found : " + name);
+        }
         return instanceMap.get(name);
     }
 
     public void put(String name, T instance) {
         if (StringUtils.isEmpty(name)) {
             throw new IllegalArgumentException("Extension name == null");
+        }
+        if (instanceMap.get(name) != null) {
+            throw new RuntimeException("duplicate instance name not found : " + name);
         }
         instanceMap.put(name, instance);
     }
